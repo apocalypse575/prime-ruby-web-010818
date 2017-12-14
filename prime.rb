ff6692cf497a72e57f7725fce25580a8884bc98c
apocalypse575 <apocalypse575@gmail.com>
2017-12-14 01:02:39 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,13 @@
 # Add  code here!
+# def prime?(num)
+#       (2..Math.sqrt(num)).each { |i| return false if num % i == 0}
+#       true
+#   end
+
 def prime?(num)
-      (2..Math.sqrt(num)).each { |i| return false if num % i == 0}
-      true
+      i = 2
+      while i < num
+        return false if num % i == 0
+      end
+      return true
   end
